1623c1836d5449078486164d198cce06d1ee05eb
Joe <33972521+hnb-ku@users.noreply.github.com>
2018-05-08 21:25:17 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,11 @@
 # name: Discourse topic excerpt plugin
 # about: A small plugin to force all topics to have excerpts
-# version: 0.1
-# authors: Angus McLeod
+# version: 0.2
+# authors: Angus McLeod, Johani
 
 after_initialize do
+  add_to_class :post, :excerpt_for_topic do
+      Post.excerpt(cooked, 50, strip_links: true)
+  end
   add_to_serializer(:listable_topic, :include_excerpt?) { true }
 end
