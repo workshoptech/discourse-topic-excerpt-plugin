c8bc1e6e3e9ab0b9cba6e3d4ae33af288bcec086
Dan <dan@sortedfood.com>
2018-11-27 12:00:36 +0000

    Remove customisation of excerpt display. Simply add excerpt by default

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,8 @@
 # name: Discourse topic excerpt plugin
 # about: A small plugin to force all topics to have excerpts
-# version: 0.2
-# authors: Angus McLeod, Johani
+# version: 0.3
+# authors: Angus McLeod, Johani, Workshop
 
 after_initialize do
-  add_to_class :post, :excerpt_for_topic do
-      Post.excerpt(cooked, 50, strip_links: true)
-  end
   add_to_serializer(:listable_topic, :include_excerpt?) { true }
 end
